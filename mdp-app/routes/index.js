87153b91241459391b2e24d6d90dc642d50ee81b
stepheniew <stephenlim2004@gmail.com>
2024-10-08 10:05:02 +0700

    update

--- mdp-app/routes/index.js
+++ mdp-app/routes/index.js
@@ -18,5 +18,54 @@ router.get('/', function(req, res, next) {
     ];
   res.render('home', { title: 'Home',berita,layout : 'main' });
 });
+router.get("/about",(req,res)=>{
+    //res.send("About Us");
+    //res.sendFile(__dirname + "/about.html");
+    res.render("about",{title:'About us',layout:'main'});
+});
+router.get("/contact",(req,res)=>{
+    //res.send("Contact");
+    //res.sendFile(__dirname + "/contact.html");
+    res.render("contact",{title:'Contact',layout:'main'});
+});
+
+// route prodi
+router.get("/prodi",(req,res)=>{
+    
+    const prodi = [
+        {
+            programStudi: "Sistem Informasi",
+            fakultas: "FIKR",
+            singkatan : "SI"
+        },
+        {
+            programStudi: "Informatika",
+            fakultas: "FIKR",
+            singkatan : "IF"
+        },
+        {
+            programStudi: "Teknik Elektro",
+            fakultas: "FIKR",
+            singkatan : "TE"
+        },
+        {
+            programStudi: "Manajemen Informatika",
+            fakultas: "FIKR",
+            singkatan : "MI"
+        },
+        {
+            programStudi: "Manajemen",
+            fakultas: "FEB",
+            singkatan : "MJ"
+        },
+        {
+            programStudi: "Akutansi",
+            fakultas: "FEB",
+            singkatan : "AK"
+        },
+   
+    ];
+    res.render('prodi',{title:'Program Studi',prodi,layout:'main'});
+});
 
 module.exports = router;
